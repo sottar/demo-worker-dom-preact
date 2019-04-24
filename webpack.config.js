1b912070953aa89907f1c650f85eeb01d2be4d95
sottar <ohrst.18@gmail.com>
2019-04-24 19:48:19 +0900

    add webpack config and npm scripts

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const outputDir = path.join(__dirname, 'dist');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.tsx'),
+  output: {
+    path: outputDir,
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+      inject: false,
+    }),
+  ],
+};
