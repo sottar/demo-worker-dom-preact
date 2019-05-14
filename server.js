1d4df4f5d9b3298c491e9a55395f391108c35190
sottar <ohrst.18@gmail.com>
2019-05-14 15:40:14 +0900

    add index.tsx

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.set('port', process.env.PORT || 8008);
@@ -15,7 +16,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.listen(app.get('port'), function() {
   console.log(`Running on http://localhost:${app.get('port')}`);
